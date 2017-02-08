b5b7c9c251c9b8b01743c808122c0398fb92749d
anhhtle <anh.ht.le@gmail.com>
2017-02-08 02:48:51 -0800

    v4

--- app.js
+++ app.js
@@ -7,83 +7,63 @@ var state = {
 }
 
 function getDataFromGoogleCivicAPI(callback) {
-    $.getJSON(state.GOOGLE_CIVIC_URL, state.query, callback);
+    //$.getJSON(state.GOOGLE_CIVIC_URL, state.query, callback);
+    $.ajax({
+        url: state.GOOGLE_CIVIC_URL,
+        data : state.query,
+        dataType: 'json',
+        success: callback,
+        error: function( data ) {
+            $('.initial-message').removeClass('hidden');
+            $('.initial-message h3').text("No result");
+        }
+    });
 }
 
 function renderGoogleCivicAPI(data) {
-    if(data.officials) {
-        $('.result-container').removeClass('hidden');
-
-//*******************senator1 ************
-        $('.senator-1-name').text(data.officials[2].name);
-        $('#senator-1-image').attr('src', data.officials[2].photoUrl);
-        $('.senator-1-number').text(data.officials[2].phones)
-        $('.senator-1-facebook').attr('href', 'https://www.facebook.com/' + data.officials[2].channels[0].id);
-        $('.senator-1-twitter').attr('href', 'https://twitter.com/' + data.officials[2].channels[1].id);
-        if(data.officials[2].party === 'Democratic') {
-            $('.senator-1-party').text(' (D)');
-            $('.senator-1').removeClass('red');
-            $('.senator-1').addClass('blue');
+    $('.initial-message').addClass('hidden');
+    var container = $('.result-container');
+    container.removeClass('hidden');
+    var title = ['', '', 'Senator ', 'Senator ', 'Congressperson '];
+    for (var i = 2; i <= 4; i++) {
+        var template = $(
+        '<li class="profile-container">' +
+            '<div class="profile">' +
+                '<h2 class="title">' + title[i] + '<span class="name">' + data.officials[i].name + '</span><span class="party"></span></h2>' +
+                '<img class="image" src="' + data.officials[i].photoUrl + '" alt="profile-pic">' +
+            '</div>' +
+            '<div class="contact hidden">' +
+                '<h3># <span class="phone">' + data.officials[i].phones + '</span></h3>' +
+                '<a href="https://www.facebook.com/' + data.officials[i].channels[0].id + '" target="_blank"><img class="facebook logo" src="images/facebook.png"></a>' +
+                '<a href="https://twitter.com/' + data.officials[i].channels[1].id + '" target="_blank"><img class="twitter logo" src="images/twitter.png"></a>' +
+            '</div>' +
+        '</li>'
+        );
+        
+        if (data.officials[i].party === 'Democratic'){
+            template.find('.party').text(' (D)');
+            template.find('.profile').addClass('blue');
         }
-        else if(data.officials[2].party === 'Republican') {
-            $('.senator-1-party').text(' (R)');
-            $('.senator-1').removeClass('blue');
-            $('.senator-1').addClass('red');
-        }
-        else
-            $('.senator-1-party').text(' (' + data.officials[3].party + ')')
-
-//*******************senator 2*****************
-        $('.senator-2-name').text(data.officials[3].name);
-        $('#senator-2-image').attr('src', data.officials[3].photoUrl);
-        $('.senator-2-number').text(data.officials[3].phones)
-        $('.senator-2-facebook').attr('href', 'https://www.facebook.com/' + data.officials[3].channels[0].id);
-        $('.senator-2-twitter').attr('href', 'https://twitter.com/' + data.officials[3].channels[1].id);
-        if(data.officials[3].party === 'Democratic') {
-            $('.senator-2-party').text(' (D)');
-            $('.senator-2').removeClass('red');
-            $('.senator-2').addClass('blue');
-        }
-        else if(data.officials[3].party === 'Republican') {
-            $('.senator-2-party').text(' (R)');
-            $('.senator-2').removeClass('blue');
-            $('.senator-2').addClass('red');
+        else if (data.officials[i].party === 'Republican'){
+            template.find('.party').text(' (R)');
+            template.find('.profile').addClass('red');
         }
         else
-            $('.senator-2-party').text(' (' + data.officials[3].party + ')')
+            template.find('.party').text(' (' + data.officials[i].party + ')');
 
-//******************congressman*****************
-        $('.congress-name').text(data.officials[4].name);
-        $('#congress-image').attr('src', data.officials[4].photoUrl);
-        $('.congress-number').text(data.officials[4].phones)
-        $('.congress-facebook').attr('href', 'https://www.facebook.com/' + data.officials[4].channels[0].id);
-        $('.congress-twitter').attr('href', 'https://twitter.com/' + data.officials[4].channels[1].id);
-        if(data.officials[4].party === 'Democratic') {
-            $('.congress-party').text(' (D)');
-            $('.congress').removeClass('red');
-            $('.congress').addClass('blue');
-        }
-        else if(data.officials[4].party === 'Republican') {
-            $('.congress-party').text(' (R)');
-            $('.congress').removeClass('blue');
-            $('.congress').addClass('red');
-        }
-        else
-            $('.congress-party').text(' (' + data.officials[4].party + ')')
-    }
-    else {
-        $('.result-div').html("No result");
+        container.append(template);
     }
 }
 
-$('.profile').on('click', function(event) {
+$('ul').on('click', 'li', function(event) {
     event.preventDefault();
-    $(this).siblings().toggleClass('hidden');
+    $(this).find('.contact').toggleClass('hidden');
 })
 
 $('.search-form').submit(function(event) {
     event.preventDefault();
     state.query.address = $(this).find('#search-input').val();
+    $('.result-container').empty();
     getDataFromGoogleCivicAPI(renderGoogleCivicAPI);
 })
 
